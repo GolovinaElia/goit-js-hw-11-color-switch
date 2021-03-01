e3a3320b1d7d89989e280630290939ba36d1a4d1
GolovinaElia <eliagolovina@ukr.net>
2021-03-01 23:52:00 +0200

    Добавляет файлы

--- js/task.js
+++ js/task.js
@@ -6,6 +6,25 @@ const colors = [
   '#009688',
   '#795548',
 ];
+// Напиши скрипт, который после нажатия кнопки Start, раз в секунду меняет цвет фона
+// body на случайное значение из массива используя инлайн - стиль.При нажатии на кнопку Stop,
+//     изменение цвета фона должно останавливаться.
+
+// ⚠️ Учти, на кнопку Start можно нажать бесконечное количество раз.Сделай так,
+//     чтобы пока изменение темы запушено, кнопка Start была не активна.
+
+// Для генерации случайного числа(индекс элемента массива цветов),
+//     используй функцию randomIntegerFromInterval.
+
+const startBtn = document.querySelector('button[data-action="start"]');
+const stopBtn = document.querySelector('button[data-action="stop"]');
+
+startBtn.addEventListener('click', () => {
+    setTimeout(() => {
+        console.log('Start');
+    }, 1000);
+});
+//  stopBtn.addEventListener('click');
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
